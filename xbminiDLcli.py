a2f2994ff6aa20f1ff123d47d123e23ba3b1ac0f
sco1 <sco1.git@gmail.com>
2018-04-18 11:47:12 -0700

    Add OSX drive parsing support

--- xbminiDLcli.py
+++ xbminiDLcli.py
@@ -10,6 +10,8 @@ from pathlib import Path
 myOS = platform.system().lower()
 if myOS.startswith('win'):
     import win32api
+elif myOS == 'darwin':
+    from os import listdir
 
 # Force UTC Timestamps
 # From the logging cookbook: https://docs.python.org/3/howto/logging-cookbook.html
@@ -22,7 +24,7 @@ logging.basicConfig(filename='./log/xbminiCLI.log', filemode='a', level=logging.
                     format=logformat, datefmt=dateformat
                     )
 
-@click.comand()
+@click.command()
 @click.option('--date', prompt=True, help='Drop Date [str:YYYYMMDD]')
 @click.option('--location', prompt=True, help='Drop Location [str]')
 @click.option('--systemname', prompt=True, help='Parachute System Name [str]')
@@ -32,6 +34,9 @@ def main(date, location, systemname, auw, nloggers):
     pass
 
 def getXBMdrives():
+    # Define XBM volume name regex
+    xbmregex = 'X\d+D\d+'
+
     if myOS.startswith('win'):
         # Pull list of drive letters
         drives = win32api.GetLogicalDriveStrings()
@@ -48,10 +53,13 @@ def getXBMdrives():
                 else:
                     raise err
                 
-        xbminidrives = [Path(drive) for drive in drivenames if re.match('X\d+D\d+', drivenames[drive])]
+        xbminidrives = [Path(drive) for drive in drivenames if re.match(xbmregex, drivenames[drive])]
+    elif myOS == 'darwin':
+        volumenames = listdir('/Volumes')
+        xbminidrives = [Path(volume) for volume in volumenames if re.match(xbmregex, volume)]
     else:
-        # TODO: Add OSX support, then Linux (if different from OSX)
-        raise NotImplementedError
+        # TODO: Add Linux (if different from OSX)
+        raise NotImplementedError(f"OS '{myOS}' not supported")
 
     return xbminidrives
 
