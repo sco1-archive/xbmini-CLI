000809bc97a72640f5aabf9d6d721d136fccffec
sco1 <sco1.git@gmail.com>
2018-04-20 13:33:15 -0700

    Add basic polling/restart flow
    Rearrange methods
    Add file transfer framework

--- xbminiDLcli.py
+++ xbminiDLcli.py
@@ -1,10 +1,11 @@
-import platform
 import logging
-import time
+import platform
 import re
+import time
+from pathlib import Path
 
 import click
-from pathlib import Path
+from tqdm import tqdm, trange
 
 # Platform specific imports
 myOS = platform.system().lower()
@@ -25,32 +26,38 @@ logging.basicConfig(filename='./log/xbminiCLI.log', filemode='a', level=logging.
                     )
 
 @click.command()
+@click.option('--outpathbase', type=Path, help='Base Output Directory')
 @click.option('--date', type=str, prompt=True, help='Drop Date [YYYYMMDD]')
 @click.option('--location', type=str, prompt=True, help='Drop Location')
 @click.option('--systemname', type=str, prompt=True, help='Parachute System Name')
 @click.option('--auw', type=float, prompt=True, help='System All Up Weight [lb]')
 @click.option('--nloggers', type=int, prompt=True, help='Number of XBM loggers')
-@click.option('--outpath', type=click.Path(), default=Path('.'), help='Base Output Directory')
 @click.option('--timeout', type=int, default=5, help='Device Polling Timeout (s)')
-def mainCLI(outpath, date, location, systemname, auw, nloggers, timeout):
-    xbmdrives = XBMpoll(timeout)
+def mainCLI(outpathbase, date, location, systemname, auw, nloggers, timeout):
+    if not outpathbase:
+        outpathbase = Path(f"./{date} {location}/{systemname}")
 
-def XBMpoll(timeout, pollinginterval=0.5):
-    click.secho('\nPolling for XBM devices...', fg='green')
-
-    pollinginterval = 0.5  # Polling interval, seconds
-    pollingrange = range(0, timeout*int(1/pollinginterval), 1)  # Good enough for a simple polling loop
-    loggerfound=False
-    with click.progressbar(pollingrange, show_percent=False, show_eta=False) as bar:
-        for item in bar:
-            xbmdrives = getXBMdrives()
-            if len(xbmdrives) == 0:
-                time.sleep(pollinginterval)
+    for ii in range(nloggers):
+        click.pause(info=f"\nPress any key to download logger #{ii+1} ... ")
+        
+        retry = True
+        while retry:
+            xbmdrives = XBMpoll(timeout)
+            if xbmdrives:
+                retry = False
+                click.secho(f"Found {len(xbmdrives)} XBM(s)", fg='green')
+                
+                # Limit file transfer to first drive found
+                if len(xbmdrives) > 1:
+                    click.secho(f"Processing volume '{xbmdrives[0]}', ignoring '{xbmdrives[1:]}''")
+                
+                sourcedrive = xbmdrives[0]
             else:
-                click.secho(f"\n Found {len(xbmdrives)} XBMs", fg='green')
-                return xbmdrives
-        else:
-            click.secho('\nNo logger(s) found', fg='red')
+                click.secho('No logger(s) found', fg='red')
+                retry = click.confirm('Retry search?')
+        
+        if xbmdrives:
+            transferdata(sourcedrive, outpathbase, ii)
 
 def getXBMdrives():
     # Define XBM volume name regex
@@ -81,6 +88,24 @@ def getXBMdrives():
         raise NotImplementedError(f"OS '{myOS}' not supported")
 
     return xbminidrives
+
+def XBMpoll(timeout, pollinginterval=0.5):
+    click.secho('Polling for XBM devices ...', fg='green')
+
+    pollinginterval = 0.5  # Polling interval, seconds
+    loggerfound=False
+    for ii in trange(0, timeout*int(1/pollinginterval), 1, bar_format='{bar}| {remaining}'):
+        xbmdrives = getXBMdrives()
+        if len(xbmdrives) == 0:
+            time.sleep(pollinginterval)
+        else:
+            logging.debug(f"Found {len(xbmdrives)} XBM(s)")
+            return xbmdrives
+    else:
+        logging.debug('No logger(s) found')
+
+def transferdata(sourcedrive, outpathbase):
+    pass
 
 if __name__ == "__main__":
     mainCLI()
